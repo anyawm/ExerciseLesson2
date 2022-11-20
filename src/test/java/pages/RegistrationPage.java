5df7725da6a7756e1103c2bcc4e846d982a1cb11
Anna Petrova <mail.anna.p@yandex.ru>
2022-11-20 23:09:57 +0500

    add page objects

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,7 +1,61 @@
 package pages;
 
-public class RegistrationPage {
-    public void openPage() {
+import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class RegistrationPage {
+    private final String titlepage = "Student Registration Form";
+    private SelenideElement
+            lastNameInput = $("#lastName"), //чтобы менять только в одном месте, если изменится написание элемента на странице
+            firstNameInput = $("#firstName"),
+            eMailInput = $("#userEmail");
+
+    public RegistrationPage openPage() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text(titlepage));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        return this;
+    }
+
+    public RegistrationPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setLastName(String value) {
+        lastNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage clearLastName() {
+        lastNameInput.clear();
+
+        return this;
+    }
+
+    public RegistrationPage setEmail(String value) {
+        eMailInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setGender(String value) {
+        $("#genterWrapper").$(byText(value)).click(); // todo move to Selenide elements
+
+        return this;
+    }
+
+    public RegistrationPage setPhone(String value) {
+        $("#userNumber").setValue(value);
+
+        return this;
     }
 }
--- src/test/java/tests/RegistrationWithPageObjectsTests.java
+++ src/test/java/tests/RegistrationWithPageObjectsTests.java
@@ -1,41 +1,58 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
-public class RegistrationWithPageObjectsTests {
+public class RegistrationWithPageObjectsTests extends TestBase {
 
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.holdBrowserOpen = true; //чтобы браузер не закрывался
-        Configuration.browserSize = "1920x1080"; // браузер определенного размера
-        Configuration.baseUrl = "https://demoqa.com"; //?
+    @Test
+    void successfulRegistrationTest() {
+        String userName = "Alex";
+
+        registrationPage.openPage()
+                .setFirstName(userName)
+                .setLastName("Егоров")
+                .setLastName("alex@egorov.com")
+                .setGender("Other")
+                .setPhone("1234567890");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2008");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click(); //если в зоне видимости две одинаковые даты разных месяцев
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").setValue("Some address 1");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(userName), text("Egorov"),
+                text("alex@egorov.com"), text("1234567890"));
     }
 
     @Test
+    void successfulRegistrationTestVer0() {
+        String userName = "Alex";
 
-     void fillFormTest() {
+        registrationPage.openPage();
 
-        String userName = "Anna";
+        registrationPage.setFirstName(userName);
+        registrationPage.setLastName("Егоров");
+        registrationPage.setLastName("alex@egorov.com");
+        registrationPage.setGender("Other");
+        registrationPage.setPhone("1234567890");
 
-        open("https://demoqa.com/text-box");
-
-        // $("[id=userName]").setValue("Anna") # заменяет id=
-        $("#userName").setValue(userName);
-        $("#userEmail").setValue("email@domain.com");
-        $("#currentAddress").setValue("Russia, Y");
-        $("#permanentAddress").setValue("Russia, SP");
-        $("#submit").click();
-
-        $("#output").shouldBe(Condition.visible);
-        $("#output #name").shouldHave(Condition.text(userName));
-        $("#output #email").shouldHave(Condition.text("email@domain.com"));
-        $("#output #currentAddress").shouldHave(Condition.text("Russia, Y"));
-        $("#output #permanentAddress").shouldHave(Condition.text("Russia, SP"));
+        $("#dateOfBirthInput").click();
+// ...
     }
 }
